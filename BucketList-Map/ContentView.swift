c08591d8b7eca3a9d4bdbe836eb3eef010015ee6
jperez1478 <jessicperez3@gmail.com>
2022-04-21 01:52:07 -0500

    Map annotations with red circle on them

--- BucketList-Map/ContentView.swift
+++ BucketList-Map/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
+
+//mock data
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+    //MARK: -Properties
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+
+        let locations = [
+            Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.141)),
+            Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+        ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { locations in
+            
+            MapAnnotation(coordinate: locations.coordinate) {
+                VStack{
+                    Circle()
+                        .stroke(.red,lineWidth: 3)
+                        .frame(width: 44, height: 44)
+                }
+              
+            }
+        
+        }
+        
+        
+    
     }
 }
 
