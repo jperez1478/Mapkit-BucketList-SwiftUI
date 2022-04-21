ffa90e248a3a6db64b590fc5db22c9b50224218d
jperez1478 <jessicperez3@gmail.com>
2022-04-21 01:31:50 -0500

    Initial Commit

---
+++ BucketList-Map/BucketList_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketList_MapApp.swift
+//  BucketList-Map
+//
+//  Created by Jessica Perez on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketList_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
